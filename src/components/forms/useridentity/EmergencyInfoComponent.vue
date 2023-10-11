3a4fa93061461ae62005c99b9be39b9d86045534
NedFalkor <matthieunedir@orange.fr>
2023-10-11 10:15:44 +0200

    ajout vmodel user + creation nemocounter

--- src/components/forms/useridentity/EmergencyInfoComponent.vue
+++ src/components/forms/useridentity/EmergencyInfoComponent.vue
@@ -3,34 +3,34 @@
 
     <div class="mb-4">
       <h3 class="text-2xl mb-5 text-center">En Cas de Nécessité</h3>
-  <h4 class="text-sm mb-2 text-center italic">Personne à Prévenir</h4>
+      <h4 class="text-sm mb-2 text-center italic">Personne à Prévenir</h4>
     </div>
 
     <div class="mb-4">
       <h3 class="text-xl mb-4 text-center">Nom de Famille</h3>
-      <input type="text" id="emergencyLastName"
+      <input v-model="emergencyLastName" type="text" id="emergencyLastName"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Prénom</h3>
-      <input type="text" id="emergencyFirstName"
+      <input v-model="emergencyFirstName" type="text" id="emergencyFirstName"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Adresse</h3>
-      <input type="text" id="emergencyAddress"
+      <input v-model="emergencyAddress" type="text" id="emergencyAddress"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
     </div>
 
     <div class="mb-4">
       <h3 class="text-xl mb-4 text-center">Téléphone Fixe</h3>
-      <input type="tel" id="emergencyLandline"
+      <input v-model="emergencyLandline" type="tel" id="emergencyLandline"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Téléphone Portable</h3>
-      <input type="tel" id="emergencyMobile"
+      <input v-model="emergencyMobile" type="tel" id="emergencyMobile"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Adresse Email</h3>
-      <input type="email" id="emergencyEmail"
+      <input v-model="emergencyEmail" type="email" id="emergencyEmail"
         class="w-full p-2 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
     </div>
   </div>
@@ -50,6 +50,7 @@ export default {
       emergencyEmail: ''
     };
   },
+
   methods: {
     // Ajoutez ici les méthodes si nécessaire.
   }
--- src/components/forms/useridentity/PersonnalInfoComponent.vue
+++ src/components/forms/useridentity/PersonnalInfoComponent.vue
@@ -1,57 +1,55 @@
 <template>
-
   <div class="bg-gray-100 w-1/2 p-6">
     <h3 class="text-2xl mb-5 text-center">Identité du plongeur</h3>
 
-  <div class="mb-4">
-    <input type="file" accept="image/*" @change="onFileSelected" 
-       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-auto block" />
-    <div v-if="imageURL" class="image-container mt-4">
-      <img :src="imageURL" alt="Chosen Image"/>
+    <div class="mb-4">
+      <input type="file" accept="image/*" @change="onFileSelected"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-auto block" />
+      <div v-if="imageURL" class="image-container mt-4">
+        <img :src="imageURL" alt="Chosen Image" />
+      </div>
     </div>
-  </div>
 
     <div class="mb-4">
       <h3 class="text-xl mb-4 text-center">Nom de Famille</h3>
-      <input type="text" id="lastName"
+      <input v-model="lastName" type="text" id="lastName"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Prénom</h3>
-      <input type="text" id="firstName"
+      <input v-model="firstName" type="text" id="firstName"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Adresse</h3>
-      <input type="text" id="address"
+      <input v-model="address" type="text" id="address"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Code Postal</h3>
-      <input type="text" id="postalCode"
+      <input v-model="postalCode" type="text" id="postalCode"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Ville</h3>
-      <input type="text" id="city"
+      <input v-model="city" type="text" id="city"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Pays</h3>
-      <input type="text" id="country"
+      <input v-model="country" type="text" id="country"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
     </div>
 
     <div class="mb-4">
       <h3 class="text-xl mb-4 text-center">Téléphone Fixe</h3>
-      <input type="tel" id="landlinePhone"
+      <input v-model="landlinePhone" type="tel" id="landlinePhone"
         class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
       <h3 class="text-xl mb-4 text-center">Téléphone Portable</h3>
-      <input type="tel" id="mobilePhone"
-      class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      <input v-model="mobilePhone" type="tel" id="mobilePhone"
+        class="w-full p-2 mb-4 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 
-    <h3 class="text-xl mb-4 text-center">Adresse Email</h3>
-    <input type="email" id="emailAddress"
-      class="w-full p-2 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      <h3 class="text-xl mb-4 text-center">Adresse Email</h3>
+      <input v-model="emailAddress" type="email" id="emailAddress"
+        class="w-full p-2 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+    </div>
   </div>
-</div>
-
 </template>
 
 
